41bb8b7cb871f5dae2fa3e7d288f2ccfc0ed196d
Aeek True <aeektrue@gmail.com>
2024-04-10 20:37:08 +0300

    gitignore

--- emotion/ui/__init__.py
+++ emotion/ui/__init__.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
-from emotion.storage import storage
+from .json_editor import json_editor
+from loguru import logger
 
 def init_ui():
     dpg.create_context()
@@ -8,21 +9,10 @@ def init_ui():
         default_font = dpg.add_font('data/fonts/OpenSans-Regular.ttf', 24)
     dpg.bind_font(default_font)
     
-    show_storage(storage)
+    json_editor()
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.maximize_viewport()
     dpg.start_dearpygui()
     dpg.destroy_context()
 
-def show_storage(storage):
-    def show_records():
-        for r in storage.get_all():
-            dpg.add_text(r)
-    with dpg.window(label=storage.get_path(), width=800, height=640):
-        with dpg.group(horizontal=True):
-            dpg.add_button(label='Refresh')
-            dpg.add_button(label='New', callback=lambda: True)
-        with dpg.group(): 
-            show_records()
-            
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from loguru import logger
-logger.add(sys.stdout, format='{message}')
+logger.add(sys.stdout, format='<lvl>{level}</> {message}')
 
 from emotion.ui import init_ui
 from emotion.storage import storage
@@ -8,7 +8,7 @@ from emotion.storage import storage
 class Card():
     parent = None
     id_ = None
-    def __init__(self, parent, properties):
+    def __init__(self, parent=None, properties=None):
         self.properties = properties
 
 def main():
